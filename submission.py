0da25ba4bf02ef77b20c216cbd37078754682c57
us579 <wzus579@gmail.com>
2019-04-23 01:24:23 +1000

    finish_viterbi

--- submission.py
+++ submission.py
@@ -1,7 +1,4 @@
 # -*- codeing:utf-8 -*-
-import numpy as np
-import pandas as pd
-from collections import defaultdict
 import math
 # Question 1
 def viterbi_algorithm(State_File, Symbol_File, Query_File): # do not change the heading of the function
@@ -9,7 +6,6 @@ def viterbi_algorithm(State_File, Symbol_File, Query_File): # do not change the 
     sym = []
     with open(State_File) as f1:
         n1 = int(f1.readline())
-        Pi = {k: 0 for k in range(n1)}
         distance = [[0 for _ in range(n1)] for _ in range(n1)]
         transition_probability = [[0 for _ in range(n1)] for _ in range(n1)]
         for i in range(n1):
@@ -20,11 +16,6 @@ def viterbi_algorithm(State_File, Symbol_File, Query_File): # do not change the 
             distance[int(i[0])][int(i[1])] = int(i[2])
         for i in range(n1):
             for j in range(n1):
-                end, begin =  states.index("END"),states.index("BEGIN")
-                if i == end or i == begin or j == end or j == begin :
-                    if i ==  begin:
-                        Pi[j] = (float(distance[i][j])) / (sum(distance[i]))
-                    continue
                 transition_probability[i][j] = (float(distance[i][j])+1) / (sum(distance[i])+n1-1)
 
     with open(Symbol_File) as f2:
@@ -44,61 +35,51 @@ def viterbi_algorithm(State_File, Symbol_File, Query_File): # do not change the 
     with open(Query_File) as f3:
         n3 = f3.readlines()
         obs = [x.strip().split() for x in n3]
-        obs = obs[1]
+    lis = []
+    for obs in obs:
+        lis.append(viterbi(states,obs,transition_probability,emission_probability,sym,distance2,n2))
+    return lis
 
-    print('obs',obs)
-    print('states',states)
-    print('sym',sym)
-    print('transition_probability',transition_probability)
-    print('emission_probability',emission_probability)
-    print('Pi',Pi)
-
+def viterbi(states,obs,transition_probability,emission_probability,sym,distance2,n2):
     path = {s:[] for s in states}
-
     curr_pro = {}
-    for s in states[:len(states)-2]:
-        curr_pro[s] = Pi[states.index(s)]*emission_probability[states.index(s)][sym.index(obs[0])]
-    print(curr_pro)
+    for s in states[:-2]:
+        curr_pro[s] = transition_probability[len(states)-2][states.index(s)]*emission_probability[states.index(s)][sym.index(obs[0])]
     for i in range(1,len(obs)):
         last_pro = curr_pro
         curr_pro = {}
-        for cur in range(len(states[:len(states)-2])):
-
+        for cur in range(len(states[:-2])):
             try:
-                max_pr,last_pr = max(((last_pro[k] * transition_probability[states.index(k)][cur]*
-                                       emission_probability[cur][sym.index(obs[i])], k) for k in states[:3]))
+                (max_pr,last_state) = max([(last_pro[k] * transition_probability[states.index(k)][cur]*
+                                       emission_probability[cur][sym.index(obs[i])], k) for k in states[:3]])
             except:
-                max_pr, last_pr = max(((last_pro[k] * transition_probability[states.index(k)][cur] *
-                                        ( 1 / obs.count(obs[i]) + n2 + 1 ) , k) for k in states[:3]))
+                (max_pr,last_state) = max([(last_pro[k] * transition_probability[states.index(k)][cur] *
+                                        (1/(sum(distance2[cur])+n2 +1)) , k) for k in states[:3]])
             curr_pro[states[cur]] = max_pr
-            path[states[cur]].append(last_pr)
-
+            path[states[cur]].append(last_state)
+    for i in states[:-2]:
+        curr_pro[i] = curr_pro[i] * transition_probability[states.index(i)][len(states)-1]
     max_pr=max(curr_pro,key=lambda x:curr_pro[x])
-    print(math.log(curr_pro[max_pr]))
-    lis_z = [3,states.index(max_pr)]
-    for i in path[max_pr]:
-        lis_z.append(states.index(i))
-    lis_z.extend([4,math.log(curr_pro[max_pr])])
-    print(lis_z)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    lis = [states[-1],max_pr]
+    for num in range(len(path[max_pr])-1,-1,-1):
+        lis.append(path[lis[-1]][num])
+    lis.append(states[-2])
+    lis = [states.index(ele) for ele in lis[::-1]]
+    lis.append(math.log(curr_pro[max_pr]))
+    # print('obs',obs)
+    # print('states',states)
+    # print('sym',sym)
+    # print('transition_probability',transition_probability)
+    # print('emission_probability',emission_probability)
+    return lis
 
 
 State_File ='./toy_example/State_File'
 Symbol_File='./toy_example/Symbol_File'
 Query_File ='./toy_example/Query_File'
 viterbi_result =viterbi_algorithm(State_File, Symbol_File, Query_File)
+for row in viterbi_result:
+    print(row)
 
 
 # Question 2
